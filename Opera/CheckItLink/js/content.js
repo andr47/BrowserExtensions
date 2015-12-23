7ba0f0e0708b8c1970cc6fd117f6e114dbc3890d
yarkovaleksei <yarkov_aleksei@mail.ru>
2015-12-23 20:31:32 +0300

    Fixed button show and hide actions. Change position to right/top point.

--- Opera/CheckItLink/js/content.js
+++ Opera/CheckItLink/js/content.js
@@ -46,9 +46,10 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse){
 });
 
 // формируем объект данных о картинке и отправляем в background.js
-function CheckItLinkClick(th){
+function CheckItLinkClick(){
 	// отменяем дефолтное поведение на случай если картинка == ссылка, чтоб не перейти при нажатии на нашу кнопку
 	event.preventDefault();
+	$(".CheckItLink_Button").hide();
 	var img = $(CURRENT_IMAGE);
 	var u = new URL(img.attr('src'));
 	var imgInfo = {
@@ -69,44 +70,51 @@ function CheckItLinkClick(th){
 
 $(document).ready(function(){
 
-	$('body').prepend("<span title='Сохранить в CheckItLink' id='CheckItLink_Button'></span>");
+	$('body').prepend("<span title='Сохранить в CheckItLink' class='CheckItLink_Button'></span>");
 
 	// минимальная ширина картинки
 	var minw = 80;
 	// минимальная высота картинки
 	var minh = 80;
 
-	$("#CheckItLink_Button").on("click", function(){
+	$(".CheckItLink_Button").on("click", function(){
 		CheckItLinkClick();
 	});
 
 	$('body')
 		.on("mouseenter", 'img', function(){
+			getOptions();
 			CURRENT_IMAGE = this;
 			var pos = getOffset(this);
 			var width = $(this).width();
 			var height = $(this).height();
 			var top = (pos.top+5);
-			var left = (pos.left+5);
+			var img = $(CURRENT_IMAGE);
+			var left = (pos.left+(img.width()-32-5));
 
 			// если картинка меньше минимальных настроек, то не обрабатываем наведение курсора
 			if(width < minw || height < minh) return false;
 			// Проверка настройки
 			if(JSOptions.showButton){
-				$("#CheckItLink_Button")
+				$(".CheckItLink_Button")
+					.stop(true, true)
 					.css({"top": top, "left": left})
 					.animate({opacity:'show'}, 300);
 			}
 		});
 	$('body')
 		.on("mouseleave", 'img', function(){
-			$("#CheckItLink_Button")
-				.animate({opacity:'hide'}, 200)
+			$(".CheckItLink_Button")
+				.animate({opacity:'hide'}, 100)
 				.stop(true, true);
 		});
+
 });
 
 $(window).on('load resize', function(){
+
+	$(".CheckItLink_Button").hide();
+
 	windowSize = {
 	    "width": $(window).width(),
 	    "height": $(window).height()
