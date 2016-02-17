84301373b7fbed52dd9832d42d10bc1dfde19543
yarkovaleksei <yarkov_aleksei@mail.ru>
2016-02-17 03:28:39 +0300

    Read code comments... Fucking cross-domen request. I am fucking him mouth )))

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -4,46 +4,27 @@
 // 
 // Содержимое сжатого файла вставить в ссылку по образцу как в файле test.html
 // ВСЕ ДВОЙНЫЕ КАВЫЧКИ В КОДЕ bookmarklet.min.js ЗАМЕНИТЬ НА ОДИНАРНЫЕ!!!!!
-function addBookmarkInCheckitLink() {
-	// создаем iframe
-	var CHECKITLINK_WIN_IFRAME = document.createElement("iframe");
-	// установим адрес нашей страницы
-	CHECKITLINK_WIN_IFRAME.src = "https://checkitlink.com/browser/bookmarklet";
-	// установим имя фрейма (потом будем по нему обращаться)
-	CHECKITLINK_WIN_IFRAME.name = "CHECKITLINK";
-	// установим функцию, которая выполнится после загрузки фрейма
-	CHECKITLINK_WIN_IFRAME.onload = timeoutLoad;
-	// и размер 0, чтоб не отображался на странице
-	CHECKITLINK_WIN_IFRAME.width = 0;
-	CHECKITLINK_WIN_IFRAME.height = 0;
-	// формируем данные для отправки
-	var LINK_DATA_OBJECT = null;
-	LINK_DATA_OBJECT = {
-					// генерируем PHP кодом на странице пользователя
-					SECRET_KEY: "7Y0GGFfjhi3Fysa2pyWoDIDV4SiUKWvt", 
-					// ну про action и говорить не стоит ))
-					action: "ADDURL", 
-					// APPID для идентификации. Можно генерить сюда SECRET_KEY и отслеживать статистику
-					APPID: "bookmarklet",
-					// собственно данные ссылки
-					args: {
-						hashtag: [],
-						categori_id: 8,
-						title: document.title,
-						description: "",
-						url: document.location.href,
-						status: 0
-					}
-				}
-	// добавляем iframe в конец текущей страницы
-	console.log("Начинаем отправку...");
-	document.body.appendChild(CHECKITLINK_WIN_IFRAME);
 
-	function timeoutLoad(){
-		// отправляем данные в наш фрейм
-		var CHECKITLINK_WIN = window.frames.CHECKITLINK;
-		CHECKITLINK_WIN.postMessage(JSON.stringify(LINK_DATA_OBJECT), "*");
-		console.log("Отправлено: %s", JSON.stringify(LINK_DATA_OBJECT));
-	}
 
+// создаем нашу переменную, которую будет искать на странице подключаемый скрипт
+CHECKITLINK_SECRET_KEY = null;
+
+// подключаем удаленный скрипт
+function addCheckitLinkScript() {
+
+	// генерируем PHP кодом на странице пользователя, перед тем как предложить установить закладку
+	CHECKITLINK_SECRET_KEY = "7Y0GGFfjhi3Fysa2pyWoDIDV4SiUKWvt";
+
+	// создаем script
+	var CHECKITLINK_WIN_SCRIPT = document.createElement("script");
+
+	// установим адрес нашего скрипта
+	// по этому адресу выводим файл bookmarklet.php (приложен в текущей папке)
+	// CHECKITLINK_WIN_SCRIPT.src = "https://checkitlink.com/browser/bookmarklet/getscript";
+	CHECKITLINK_WIN_SCRIPT.src = "https://randomfio.xyz/test/bookmarklet.php?c="+Date.now()+"&getscript";
+
+	// добавляем script в head текущей страницы
+	document.head.appendChild(CHECKITLINK_WIN_SCRIPT);
 }
+
+addCheckitLinkScript();
--- bookmarklet/bookmarklet.php
+++ bookmarklet/bookmarklet.php
@@ -1,7 +1,99 @@
+<?php
+
+if(isset($_GET["getscript"])) {
+	
+	header("Content-Type: application/javascript; charset=utf-8");
+
+?>
+
+/* JavaScript */
+
+function addBookmarkInCheckitLink() {
+
+	// в релизе убрать строку ниже
+	console.log("CHECKITLINK_SECRET_KEY: ", CHECKITLINK_SECRET_KEY);
+
+	// если не удалось найти window.CHECKITLINK_SECRET_KEY на текущей странице
+	// значит что-то не так и лучше от греха не продолжать
+	if(!window.CHECKITLINK_SECRET_KEY) {
+		// в релизе убрать строку ниже
+		console.error("CHECKITLINK_SECRET_KEY не найден на странице!");
+		return false;
+	}
+
+	// создаем iframe
+	var CHECKITLINK_WIN_IFRAME = document.createElement("iframe");
+
+	// установим адрес нашего фрейма
+	CHECKITLINK_WIN_IFRAME.src = "https://checkitlink.com/browser/bookmarklet";
+
+	// установим имя фрейма (потом будем по нему обращаться)
+	CHECKITLINK_WIN_IFRAME.name = "CHECKITLINK";
+
+	// установим функцию, которая выполнится после загрузки фрейма
+	CHECKITLINK_WIN_IFRAME.onload = timeoutLoad;
+
+	// и размер 0, чтоб не отображался на странице
+	CHECKITLINK_WIN_IFRAME.width = 0;
+	CHECKITLINK_WIN_IFRAME.height = 0;
+
+	// формируем данные для отправки
+	var LINK_DATA_OBJECT = null;
+	LINK_DATA_OBJECT = {
+					// генерируем PHP кодом на странице пользователя
+					SECRET_KEY: window.CHECKITLINK_SECRET_KEY, 
+					// ну про action и говорить не стоит ))
+					action: "ADDURL", 
+					// APPID для идентификации. Можно генерить сюда SECRET_KEY и отслеживать статистику
+					APPID: "bookmarklet",
+					// собственно данные ссылки
+					args: {
+						hashtag: [],
+						// id категории "Другое"
+						categori_id: 8,
+						title: document.title,
+						description: "",
+						url: document.location.href,
+						status: 0
+					}
+				}
+
+	// в релизе убрать строку ниже
+	console.log("Начинаем отправку...");
+	
+	// добавляем iframe в конец текущей страницы
+	document.body.appendChild(CHECKITLINK_WIN_IFRAME);
+
+	// функция методом postMessage отправит объект данных в фрейм
+	function timeoutLoad(){
+		// отправляем данные в наш фрейм
+		var CHECKITLINK_WIN = window.frames.CHECKITLINK;
+		CHECKITLINK_WIN.postMessage(JSON.stringify(LINK_DATA_OBJECT), "*");
+
+		// в релизе убрать строку ниже
+		console.log("Отправлено: %s", JSON.stringify(LINK_DATA_OBJECT));
+	}
+
+
+}
+
+// собственно вызов функции =)
+addBookmarkInCheckitLink();
+
+/* JavaScript */
+
 <?php 
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true ");
+	exit; 
+
+}
+
+?>
+
+<?php 
+
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Credentials: true ");
 
 ?>
 
